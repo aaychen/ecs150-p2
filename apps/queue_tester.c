ce957927895cf3a83dde61a44c9c89fe430b8570
aaychen <106anna@gmail.com>
2022-02-05 15:07:53 -0800

    iterate test added from project description

--- apps/queue_tester.c
+++ apps/queue_tester.c
@@ -145,15 +145,62 @@ void test_destroy(void)
     TEST_ASSERT(queue_destroy(q) == 0);
 }
 
+/* Callback function that increments integer items by a certain value (or delete
+ * item if item is value 42) */
+static int inc_item(queue_t q, void *data, void *arg)
+{
+    int *a = (int*)data;
+    int inc = (int)(long)arg;
+
+    if (*a == 42)
+        queue_delete(q, data);
+    else
+        *a += inc;
+
+    return 0;
+}
+
+/* Callback function that finds a certain item according to its value */
+static int find_item(queue_t q, void *data, void *arg)
+{
+    int *a = (int*)data;
+    int match = (int)(long)arg;
+    (void)q; //unused
+
+    if (*a == match)
+        return 1;
+
+    return 0;
+}
+
 /* Test iterating over the queue */
 void test_iterate(void)
 {
-    // TODO
 	fprintf(stderr, "*** TEST iterate ***\n");
 
-    // int data1 = 10, data2 = 20, data3 = 30, *ptr;
-    queue_t q = queue_create();
+    queue_t q;
+    int data[] = {1, 2, 3, 4, 5, 42, 6, 7, 8, 9};
+    size_t i;
+    int *ptr;
 
+    /* Initialize the queue and enqueue items */
+    q = queue_create();
+    for (i = 0; i < sizeof(data) / sizeof(data[0]); i++)
+        queue_enqueue(q, &data[i]);
+
+    /* Add value '1' to every item of the queue, delete item '42' */
+    queue_iterate(q, inc_item, (void*)1, NULL);
+    TEST_ASSERT(data[0] == 2);
+    TEST_ASSERT(queue_length(q) == 9);
+
+    /* Find and get the item which is equal to value '5' */
+    ptr = NULL;     // result pointer *must* be reset first
+    queue_iterate(q, find_item, (void*)5, (void**)&ptr);
+    TEST_ASSERT(ptr != NULL);
+    TEST_ASSERT(*ptr == 5);
+    TEST_ASSERT(ptr == &data[3]);
+
+    /* Null cases */
     TEST_ASSERT(queue_iterate(NULL, NULL, NULL, NULL) == -1);
     TEST_ASSERT(queue_iterate(q, NULL, NULL, NULL) == -1);
 }
