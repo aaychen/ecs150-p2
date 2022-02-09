e508f7539bf07e880cd9d316d678e25248c60e12
aaychen <106anna@gmail.com>
2022-02-08 17:25:33 -0800

    new test cases

--- apps/uthread_tester.c
+++ apps/uthread_tester.c
@@ -24,12 +24,13 @@ int hello(void)
 	return 0;
 }
 
-int ret_not_zero(void)
+int five(void)
 {
-	printf("%s:%d: Returning -5\n", __FILE__, __LINE__);
-    return -5;
+	printf("%s:%d: Returning 5\n", __FILE__, __LINE__);
+    return 5;
 }
 
+/* Test creation of a single thread and collection of return value */
 void test_single_thr(void)
 {
     fprintf(stderr, "*** TEST single_thr ***\n");
@@ -39,30 +40,75 @@ void test_single_thr(void)
 
 	uthread_start(0);
 	tid = uthread_create(hello);
-	uthread_join(tid, &retval);
+	TEST_ASSERT(uthread_join(tid, &retval) == 0);
     TEST_ASSERT(retval == 0);
-    uthread_stop();
+
+    tid = uthread_create(five);
+    TEST_ASSERT(uthread_join(tid, &retval) == 0);
+    TEST_ASSERT(retval == 5);
+
+    TEST_ASSERT(uthread_stop() == 0);
 }
 
-void test_single_thr_nonzero_retval(void)
+int mjr_thr2(void)
 {
-    fprintf(stderr, "*** TEST single_thr_nonzero_retval ***\n");
+    int retval = -10;
+    TEST_ASSERT(uthread_join(1, &retval) == -1);
+    TEST_ASSERT(retval == -10);
+    uthread_yield();
+    uthread_yield();
+    return 2;
+}
+
+int mjr_thr1(void)
+{
+    uthread_yield();
+    return 1;
+}
+
+/**
+ * Tests two threads joining one (not main) thread
+ * In the end, main thread will have collected the two threads.
+ */
+void test_multiple_joining_thr(void)
+{
+    fprintf(stderr, "*** TEST multiple_joining_thr ***\n");
+
+	uthread_t tid1, tid2;
+	int retval1, retval2;
+
+	uthread_start(0);
+    tid1 = uthread_create(mjr_thr1);
+    tid2 = uthread_create(mjr_thr2);
+	uthread_join(tid1, &retval1);
+    TEST_ASSERT(retval1 == 1);
+    uthread_join(tid2, &retval2);
+    TEST_ASSERT(retval2 == 2);
+    TEST_ASSERT(uthread_stop() == 0);
+}
+
+/**
+ * Tests collecting from an already dead (zombie) thread
+ */
+void test_collect_dead_thr(void) {
+    fprintf(stderr, "*** TEST collect_dead_thr ***\n");
 
 	uthread_t tid;
 	int retval;
 
 	uthread_start(0);
-	tid = uthread_create(ret_not_zero);
-	uthread_join(tid, &retval);
-    TEST_ASSERT(retval == -5);
-    uthread_stop();
+	tid = uthread_create(hello);
+    uthread_yield();
+	TEST_ASSERT(uthread_join(tid, &retval) == 0);
+    TEST_ASSERT(retval == 0);
+    TEST_ASSERT(uthread_stop() == 0);
 }
 
 int thread3(void)
 {
 	uthread_yield();
     printf("%s:%d: thread%d\n", __FILE__, __LINE__, uthread_self());
-	return 0;
+	return 3;
 }
 
 int thread2(void)
@@ -70,33 +116,36 @@ int thread2(void)
 	uthread_create(thread3);
 	uthread_yield();
 	printf("%s:%d: thread%d\n", __FILE__, __LINE__, uthread_self());
-	return 0;
+	return 2;
 }
 
 int thread1(void)
 {
-	uthread_create(thread2);
-	uthread_yield();
+    int retval;
+    uthread_join(uthread_create(thread2), &retval);;
+	uthread_yield(); // thread1 yielded to thread2
 	printf("%s:%d: thread%d\n", __FILE__, __LINE__, uthread_self());
 	uthread_yield();
-	return 10;
+	return 1;
 }
 
+/* Test creating multiple threads and collection of return value */
 void test_multiple_thr(void)
 {
     fprintf(stderr, "*** TEST multiple_thr ***\n");
 	int retval;
 
 	uthread_start(0);
-	uthread_join(uthread_create(thread1), &retval);
-    TEST_ASSERT(retval == 10);
+	uthread_join(uthread_create(thread1), &retval); // main yielded to thread1
+    TEST_ASSERT(retval == 1);
     uthread_stop();
 }
 
 int main(void)
 {
     test_single_thr();
-    test_single_thr_nonzero_retval();
-    test_multiple_thr();
+    test_multiple_joining_thr();
+    test_collect_dead_thr();
+    // test_multiple_thr();
 	return 0;
 }
